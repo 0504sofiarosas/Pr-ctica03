de80e8663ed1f1e748aecf9797d29907d509e6fc
Santiago Garduño Reyes <142323662+Aurumcat24@users.noreply.github.com>
2023-12-02 23:50:56 -0600

    Ejercicio K y M y tag de ejercicios
    Se agregan los ejercicios K y M, además se le añade la etiqueta a los ejercicios ya existentes y se ordenaron alfabéticamente.

--- Ejercicio02/ConsultasPractica3.sql
+++ Ejercicio02/ConsultasPractica3.sql
@@ -1,21 +1,37 @@
+--Ejercicio A
 select snombreproducto, nprecioproducto, scolorproducto, stamanoproducto
 from productos;
 
+--Ejercicio B
 select snombreproducto, nprecioproducto, scolorproducto, sdetallesproducto, stamanoproducto
 from productos
 where stamanoproducto = 'Mediano';
 
+--Ejercicio C
+select snombre, sgenero, nedad, susuario, scontrasena
+from clientes;
+
+--Ejercicio D
+select *
+from clientes
+where nedad <> 24;
+
+--Ejercicio E
+select sdescripcion
+from ccodigosproducto;
+
+--Ejercicio F
 SELECT scolorproducto, AVG(nprecioproducto) as promedio_precio
 FROM productos
 WHERE scolorproducto = 'Amarillo'
 GROUP BY scolorproducto;
 
-select snombre, sgenero, nedad, susuario, scontrasena
-from clientes;
+--Ejercicio K
+SELECT sNombre, nEdad
+FROM Clientes 
+WHERE (sApellidoPaterno = 'Pérez') or (sApellidoMaterno = 'Pérez');
 
-select sdescripcion
-from ccodigosproducto;
-
-select *
-from clientes
-where nedad <> 24;
+--Ejercicio M
+SELECT sNombre, sApellidoPaterno, sCorreoElectronico, sGenero, sNumeroTelefonico
+FROM Clientes 
+WHERE (sApellidoPaterno LIKE 'S%') and (sApellidoMaterno LIKE '%a%');
